375e4c1ae034d374478ca05093299e8b4918b6b1
bibekbee <bibekbee23@gmail.com>
2024-06-17 23:09:00 +0545

    Unauthorized and login

--- resources/views/login/index.php
+++ resources/views/login/index.php
@@ -5,17 +5,26 @@
       <div>
         <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
         <div class="mt-2.5 mb-2">
-          <input type="email" name="email" id="email" value="<?= $input['email'] ?? ''?>" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-<?= isset($errors['email']) ? 'red' : 'gray' ?>-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input type="email" name="email" id="email" value="<?= $input['email'] ?? ''?>" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
-        <span class="text-xs text-red-500"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
+ 
       </div>
 
       <div>
         <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Password</label>
         <div class="mt-2.5 mb-2">
-          <input type="password" name="password" id="password" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-<?= isset($errors['password']) ? 'red' : 'gray' ?>-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input type="password" name="password" id="password" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
+ 
+      </div>
+
+      <div class="-mt-4">
+        <?php if(isset($errors['email']) && isset($errors['password'])) : ?>
+        <span class="text-xs text-red-500"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
+        <?php else : ?>
+        <span class="text-xs text-red-500"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
         <span class="text-xs text-red-500"><?= isset($errors['password']) ? $errors['password'] : '' ?></span>
+        <?php endif ?>
       </div>
 
       <div class="col-span-2">
---
+++ resources/views/unauthorized.php
@@ -0,0 +1,5 @@
+<?php view('components/header.php') ?>
+
+<main class="mx-5 lg:mx-32 mt-10">
+    <h1 class="text-red-600">You have to login to view this page</h1>
+</main>
