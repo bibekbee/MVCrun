ebc3aff874ecc68e0b60a2893d565335cb9cec7f
bibekbee <bibekbee23@gmail.com>
2024-06-17 20:22:19 +0545

    nav menu and routes

--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -16,11 +16,24 @@
             </div>
 
             <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
+            <div class="flex space-x-4 items-center">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="/" class="rounded-md <?= $_SERVER['REQUEST_URI'] == '/' ? 'bg-gray-900' : '' ?> px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</a>
                 <a href="/about" class="rounded-md <?= $_SERVER['REQUEST_URI'] == '/about' ? 'bg-gray-900' : '' ?> px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                 <a href="/contact" class="rounded-md <?= $_SERVER['REQUEST_URI'] == '/contact' ? 'bg-gray-900' : '' ?> px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                <?php if(!isset($_SESSION['user'])) : ?>
+                <a href="/login" class="rounded-md <?= $_SERVER['REQUEST_URI'] == '/login' ? 'bg-gray-900' : '' ?> px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Login</a>
+                <a href="/register" class="rounded-md <?= $_SERVER['REQUEST_URI'] == '/register' ? 'bg-gray-900' : '' ?> px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Register</a>
+                <?php endif; ?>
+                <?php if(isset($_SESSION['user'])) : ?>
+                  <div class="h-full">
+                  <form action="/logout" method="post" class="my-auto">
+                    <div class="rounded-md px-3 py-2 text-sm cursor-pointer font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                    <input class="cursor-pointer" type="submit" value="Logout"/>
+                    </div>
+                  </form>
+                  </div>
+                 <?php endif; ?>
             </div>
             </div>
       
--- web.php
+++ web.php
@@ -2,6 +2,8 @@
 use app\Core\Application;
 use app\Http\Controller\Navcontroller;
 use app\Http\Controller\Contactcontroller;
+use app\Http\Controller\Logincontroller;
+use app\Http\Controller\Registercontroller;
 use app\Core\Session;
 
 
@@ -10,6 +12,11 @@ $app->router->get('/', [Navcontroller::class, 'index']);
 $app->router->get('/about', [Navcontroller::class, 'about']); 
 $app->router->get('/contact', [Contactcontroller::class, 'index']);
 $app->router->post('/contact', [Contactcontroller::class, 'store']); 
+$app->router->get('/login', [Logincontroller::class, 'index']); 
+$app->router->post('/login', [Logincontroller::class, 'store']); 
+$app->router->get('/register', [Registercontroller::class, 'index']); 
+$app->router->post('/register', [Registercontroller::class, 'store']); 
+$app->router->post('/logout', [Logincontroller::class, 'destroy']); 
 
 $app->router->resolve();
 Session::clearFlash();
