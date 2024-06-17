3147bca467227b9df63658658850540266db8948
bibekbee <bibekbee23@gmail.com>
2024-06-17 20:32:17 +0545

    Login Controller and view

---
+++ Http/Controller/Logincontroller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\Http\Controller;
+
+use app\Core\Request;
+use app\Core\Application;
+use app\Http\Models\Usermodel;
+use app\Core\Auth;
+use app\Core\Session;
+
+
+class Logincontroller extends Controller{
+    public function index(){ 
+        return view('login/index.php', ['errors' => Session::getflash('errors') , 'input' => Session::getflash('input')]);
+    }
+
+    public function store(Request $request){
+        $request->table = 'user';
+        $result = $request->validate([
+            'email' => 'required|valid|exist',
+            'password' => 'required|valid',
+        ]);
+       
+        if($result){
+            $attempt = Auth::attempt($result['email'], $result['password'], $request->table);
+            if($attempt){
+                Auth::login();
+            }
+            redirect('/');
+        }
+
+        Session::flash('input', $request->input);
+        redirect('login');
+    }
+
+    public function destroy(){
+        Auth::logout();
+    }
+}
+
+
+
