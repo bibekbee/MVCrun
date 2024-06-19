e556b81a1caeb97f95b04aaaedb3520c05b499a4
bibekbee <bibekbee23@gmail.com>
2024-06-19 21:39:51 +0545

    update

--- resources/views/profile.php
+++ resources/views/profile.php
@@ -4,5 +4,14 @@
     <div>
         <p>Welcome <?= $_SESSION['user'] ?></p>
     </div>
+    <hr>
+            <?php foreach($contacts as $contact) : ?>
+                <div class="my-2 py-4">
+                <p><?=$contact['first_name']?></p>
+                <p><?=$contact['last_name']?></p>
+                <p><?=$contact['email']?></p>
+                <hr>
+                </div>
+            <?php endforeach; ?>
 </main>
 
